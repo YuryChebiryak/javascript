ed099076221ed527b4768cf9060014ff8fc11d52
Yury Chebiryak <yury@chebiryak.name>
2019-08-13 21:45:50 +0200

    working on week5

--- course_1/week5/index.js
+++ course_1/week5/index.js
@@ -1,12 +1,27 @@
 module.exports = {
-
+    subscriptions = {},
+    subscription : function(e, s, h) {
+        return {
+            event:      e,
+            subscriber: s,
+            handler:    h
+        }
+    },
     /**
      * @param {String} event
      * @param {Object} subscriber
      * @param {Function} handler
      */
     on: function (event, subscriber, handler) {
-
+        if (this.subscriptions.hasOwnProperty(event)) {
+            var arr = this.subscription[event]
+            arr.push(new subscription(event, subscriber, handler))
+            for (let s of this.subscriptions[event]) {
+                console.log(s)
+            }
+        } else {
+            this.subscription[event] = [new this.subscription(event, subscriber, handler)]
+        }
     },
 
     /**
