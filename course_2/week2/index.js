bba1a74826550aa1444390f3088116130939732c
Yury Chebiryak <yury@chebiryak.name>
2019-08-17 07:54:20 +0200

    week2 done

--- course_2/week2/index.js
+++ course_2/week2/index.js
@@ -4,15 +4,53 @@ module.exports = Collection;
  * Конструктор коллекции
  * @constructor
  */
-function Collection() {}
+function Collection() {
+    this._values = [];
+    //return new Collection();
+}
 
 
 // Методы коллекции
-Collection.prototype.values = function () {};
+Collection.prototype.values = function () {
+    return this._values;
+};
 // другие методы
+Collection.prototype.at = function(index) {
+    if (index > this._values.length || index < 1) {
+        return null;
+    }
+    return this._values[index - 1];
+}
 
+Collection.prototype.count = function() {
+    return this._values.length;
+}
 
+Collection.prototype.removeAt = function(index) {
+    if (index > this._values.length || index < 1) {
+        return false;
+    }
+    this._values.splice(index - 1, 1);
+    return true;
+}
+
+Collection.prototype.append = function(elem) {
+    if (!(elem instanceof Collection)) {
+        this._values.push(elem);
+    } else {
+        var values = elem.values();
+        for (value of values) {
+            this.append(value);
+        }
+    }
+}
 /**
  * Создание коллекции из массива значений
  */
-Collection.from = function () {};
+Collection.from = function (other) {
+    var res = new Collection();
+    for (elem of other) {
+        res.append(elem);
+    }
+    return res;
+};
