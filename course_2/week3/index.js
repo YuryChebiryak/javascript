4ba715591ad1719ef64c1d6b35388bcf42e3a04e
Yury Chebiryak <yury@chebiryak.name>
2019-08-17 10:49:58 +0200

    week3

--- course_2/week3/index.js
+++ course_2/week3/index.js
@@ -3,5 +3,14 @@
  * @param {Function} callback
  */
 module.exports = function (operations, callback) {
-
+    if (operations.length === 0) {
+        return [];
+    }
+    Promise.all(operations)
+        .then(function(data) {
+                callback(null, data)
+            }, function() {
+                callback(new Error("error"));
+            });
+        
 };
